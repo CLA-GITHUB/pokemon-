4da045b94fe08fa9ff71b4ceaa20bf62eabeac05
CLA-GITHUB <christwealthla@gmail.com>
2023-01-22 19:28:23 +0100

    removed shimmer

--- src/components/pokemonList.tsx
+++ src/components/pokemonList.tsx
@@ -1,8 +1,11 @@
 import { usePokemon } from "../context/PokemonContext";
-import { Image, Shimmer, Breathing } from "react-shimmer";
+import { useState } from "react";
 import { Link } from "@tanstack/react-location";
+
 export default function PokemonList() {
   const { pokemon } = usePokemon();
+  const [, setLoad] = useState(true);
+
   return (
     <div>
       <div className='grid grid-cols-3 gap-2'>
@@ -10,14 +13,20 @@ export default function PokemonList() {
           <Link key={pokemon.id} to={`/pokemon/${pokemon.id}`}>
             <div className='shadow-md rounded p-3'>
               {
-                <Image
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-                  fallback={
-                    <div className='w-32 mx-auto rounded-full'>
-                      <Shimmer width={128} height={128} />
-                    </div>
-                  }
-                />
+                <>
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+                    onLoad={(e) => {
+                      setLoad(false);
+                    }}
+                    onLoadStart={() => (
+                      <div className='animate-pulse flex space-x-4'>
+                        <div className='rounded bg-slate-300 h-32 w-32 mx-auto'></div>
+                      </div>
+                    )}
+                    alt='pokemon image'
+                  />
+                </>
               }
               <p className='text-center font-semibold text-xl'>
                 {pokemon.name}
